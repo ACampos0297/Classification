585d48854716fae5b879024fabf069100556ac15
Abdul Campos <abdulcampos02@gmail.com>
2020-04-13 12:10:42 -0500

    finished knn

--- src/knn.py
+++ src/knn.py
@@ -5,7 +5,7 @@ from utils import evaluate
 import os, sys
 import string
 
-K = 5
+K = 1
 
 class KNN:
 	def __init__(self):
@@ -30,7 +30,7 @@ class KNN:
 					docfrec[word] += 1
 				else:
 					docfrec[word] = 1
-			self.bow.append((d[0],docfrec))
+			self.bow.append((d[2], docfrec))
 
 	def predict(self, x):
 		"""
@@ -49,11 +49,21 @@ class KNN:
 				docfrec[word] += 1.0
 			else:
 				docfrec[word] = 1.0
+
 		docfrecsq = dict()
 		for word in docfrec:
-			docfrecsq[word] = math.pow(docfrec[word],2)
-		
-		return Labels(0)
+			docfrecsq[word] = math.pow(docfrec[word], 2)
+
+		scores = dict()
+		for doc in self.bow:
+			similarity = sum(docfrec[key]*doc[1].get(key, 0) for key in docfrec)
+			bsq = math.sqrt(sum(math.pow(doc[1].get(key, 0), 2) for key in docfrecsq))
+			asq = math.sqrt(sum(docfrecsq.values()))
+			similarity = similarity/float(float(asq)*bsq)
+			scores[doc[0]] = similarity
+
+		sortedscores = sorted(scores.items(), key=lambda x: x[1])
+		return Labels(sortedscores[len(sortedscores)-1][0])
 
 def main(train_split):
 	knn = KNN()
@@ -64,7 +74,7 @@ def main(train_split):
 	# Evaluate the trained model on training data set.
 	print('-'*20 + ' TRAIN ' + '-'*20)
 	evaluate(knn, ds)
-"""
+
 	# Evaluate the trained model on validation data set.
 	print('-'*20 + ' VAL ' + '-'*20)
 	evaluate(knn, val_ds)
@@ -76,7 +86,7 @@ def main(train_split):
 		print('\n' + '-'*20 + ' TEST ' + '-'*20)
 		test_ds = Dataset('test').fetch()
 		evaluate(knn, test_ds)
-"""
+
 
 if __name__ == "__main__":
 	train_split = 'train'
